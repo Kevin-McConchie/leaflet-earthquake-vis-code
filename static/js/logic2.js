0323aa1a7e90b618e0513cb782a53786729c78ad
Kevin McConchie <Kevin.McConchie@outlook.com>
2022-09-01 17:34:04 +1000

    Update

---
+++ static/js/logic2.js
@@ -0,0 +1,56 @@
+// Creating map object
+var map = L.map("map", {
+    center: [40.7128, -74.0059],
+    zoom: 2
+});
+// Adding tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+}).addTo(map);
+
+
+// Loop thru the features and create one marker for each place object
+function colors(magnitude) {
+    var color = "";
+    if (magnitude <= 1) {
+        return color = "#83FF00";
+    }
+    else if (magnitude <= 2) {
+        return color = "#FFEC00";
+    }
+    else if (magnitude <= 3) {
+        return color = "#ffbf00";
+    }
+    else if (magnitude <= 4) {
+        return color = "#ff8000";
+    }
+    else if (magnitude <= 5) {
+        return color = "#FF4600";
+    }
+    else if (magnitude > 5) {
+        return color = "#FF0000";
+    }
+    else {
+        return color = "#ff00bf";
+    }
+}
+
+// Grabbing our GeoJSON data..
+d3.json(link).then(function(data) {
+    // Creating a geoJSON layer with the retrieved data
+    L.geoJson(data, {
+      // Style each feature (in this case a country)
+      style: function(feature) {
+        return {
+          color: "white",
+          // Call the chooseColor function to decide which color to color our country (color based on country)
+          fillColor: chooseColor(feature.properties.name),
+          fillOpacity: 0.5,
+          weight: 1.5
+        };
+      },
